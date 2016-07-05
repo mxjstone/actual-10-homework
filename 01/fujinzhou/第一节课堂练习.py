0b5067b4f397fdb5f7e312a1c0f434fa8eeae37d
fujinzhou <1445675350@qq.com>
2016-07-05 15:28:45 +0800

    Update and rename 第一节课练习.py to 第一节课堂练习.py

--- 01/fujinzhou/第一节课堂练习.py
+++ 01/fujinzhou/第一节课堂练习.py
@@ -36,3 +36,31 @@ h='good'
 print 'Hi %s I am %d years old ,You are very %s'%(x,y,h)
 print 'Hi '+ x +',You are very '+ h +'' 
 print ('Hi {0} I am {1} years old,You are very {2}!'.format(x,y,h))
+#布尔值 True False  与或非 
+#and
+#两边都是真，才是真
+#print True and True
+#print 2>3 and 3>2
+#or 或 ,两种情况只要有一种情况
+#print True or False
+#not 非
+#print not False
+#print not True
+
+#流程控制
+#if True or False:
+#    是True 就执行这段代码
+
+if 2<3 or 5<4:
+	print 'condition is True'
+else:
+	print 'condition is False'
+
+name='reboot'
+age=20
+if name=='reboot':
+	if age>10:
+		print 'you are %s years old'%(age)
+	print 'condition is reboot'
+else:
+	print 'condition is False'
