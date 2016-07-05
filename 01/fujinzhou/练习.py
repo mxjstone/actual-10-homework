1cb596d700e6c938c778fc77a4bebc780bec8ead
fujinzhou <1445675350@qq.com>
2016-07-05 12:05:03 +0800

    Create 练习.py

---
+++ 01/fujinzhou/练习.py
@@ -0,0 +1,29 @@
+#coding:utf-8
+#呵呵
+#print 'hello world'
+#x='hello world'
+#print x
+#x=raw_input('hello world')
+#print x
+#int
+#print 2+3
+#print 1+2*3
+#print 8/3
+#print 8/3.0
+#print 8%3
+#str  单双引号无却别
+print 'hello world'
+print "hello world"
+print "I'am pythoner"
+print 'I\'am pythoner'
+#三个单引号 起声明作用
+print '''
+this 
+
+is 
+a
+ test
+'''
+#字符串拼接
+print 'hello '+'reboot'
+print 'hello '*2
