241db56bcd4641eaae80d9c39b8ffdf95cc4b0a6
fujinzhou <1445675350@qq.com>
2016-07-07 21:34:23 +0800

    learn two day

--- 01/fujinzhou/two_day_2.py
+++ 01/fujinzhou/two_day_2.py
@@ -1,12 +1,18 @@
+#coding:utf-8
 list1=[]
+count=0
+num=0
 while True:
  x=raw_input('please input your name:')
  y=raw_input('please input your grade:')
  if y.isdigit() and x.isalpha():
     list1.append(str(x))
-    list1.append(y)
+    list1.append(int(y))
+    count+=1
+    num=(int(num)+int(y))/count
  elif len(x)==0 or len(y)==0:
     print list1
     break
  else:
     print 'input is error'
+print '平均值是 %s' %(num)
