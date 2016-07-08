b18860d354315d34f8bbcb559c2793da712edc91
fujinzhou <1445675350@qq.com>
2016-07-08 15:14:10 +0800

    two day homework

--- 02/fujinzhou/two_day.py
+++ 02/fujinzhou/two_day.py
@@ -18,3 +18,27 @@ while True:
  else:
     print 'input is error'
 print '平均值是 %s' %(avg)
+
+#list最重要的一个特点：有序。 通过索引获取值   列表[索引]
+#索引前面是从0开始，后面是从-1 开始
+
+#list 是一个内置函数，不要以它命名
+#arr = ['C','python','js','css','html','node']
+#print arr[-6]
+#arr1=list('python')
+#print arr1
+#for i in ['wd','pc','wn']:
+#	print i
+#print 'wd' in ['wd','pc'] 
+#print 'wn' in ['wd','pc'] 
+arr=[1,2,3,4,5,6]
+#print len(arr)
+#print max(arr)
+#print min(arr)
+#len(列表)返回列表的长度 min 最小值，max最大值
+del arr[3]
+print arr
+#修改 根据索引找到值，并重新赋值
+arr[0]='hello'
+print arr
+
---
+++ 02/fujinzhou/zuoye.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+#冒泡排序
+#  挨个对比，如果 一个元素比右边的大，交换位置
+arr=[3,4,8,9,10,6,5,7]
+length=len(arr)
+for i in range(length):
+	for j in range(length-1):
+		if arr[j]>arr[j+1]:
+			arr[j],arr[j+1]=arr[j+1],arr[j]
+print arr
