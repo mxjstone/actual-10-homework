16b965c9ecc0aa0f11d4448d086d213c384df5a1
fanmiao1 <83098148@qq.com>
2016-07-09 00:39:49 +0600

    homework

---
+++ 01/fanmiao/7.2homework.py
@@ -0,0 +1,14 @@
+a = [1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45,5000]
+x = 0
+y = 0
+for i in a:
+        if i > x:
+                x = i
+
+for h in a:
+        if h >y and h !=x:
+                y = i
+
+print "The first number is: %s" % x
+print "The second numer is: %s" % y
+~                                  
