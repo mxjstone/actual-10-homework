239a1f1df8f34aa3208cd35fca2a87f525035225
fujinzhou <1445675350@qq.com>
2016-07-08 15:20:27 +0800

    two_day_homework

--- 02/fujinzhou/zuoye.py
+++ 02/fujinzhou/zuoye.py
@@ -5,7 +5,7 @@
 arr=[3,4,8,9,10,6,5,7]
 length=len(arr)
 for i in range(length):
-	for j in range(length-1):
+	for j in range(length-1-i):
 		if arr[j]>arr[j+1]:
 			arr[j],arr[j+1]=arr[j+1],arr[j]
 print arr
