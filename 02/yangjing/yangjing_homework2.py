0884166f60809bebac3dcc22d82ab98620ac90b1
jyang3 <john.yang@activenetwork.com>
2016-07-13 01:23:48 +0800

    new add

--- 02/yangjing/yangjing_homework2.py
+++ 02/yangjing/yangjing_homework2.py
@@ -1,28 +1,30 @@
 # SelectionStore
 
 arr = [13,15,37,89,60,39,12,109,56,72]
-arr_length = len(arr)
 n = 0
 j = 0
 for i in range(arr_length):
-	print 'Compare round %s' % i
-	print arr[j:arr_length]
+	print '++++Compare round %s ++++' %(i+1)
 
 	for j in range(i+1, arr_length):
-		print 'compare %s with %s' %(arr[i], arr[j])
+		print 'compare number is %s' %arr[i]
+		print 'neighbor is %s' %arr[j]
+		print 'compare scope: %s' % arr[j:arr_length]
+		print 'compare %s with %s' % (arr[i], arr[j])
 		if arr[i] < arr[j]:
 			print 'no swap'
-			print arr
+			print 'no swap arr is : %s' % arr
 		else:
 			print 'swap %s  with %s' % (arr[i], arr[j])
 			arr[i], arr[j] = arr[j], arr[i]
-			print arr
-			print ' new arr min number is %s' % arr[i]
+			print 'new arr is %s' %arr
+			print 'new arr min number is %s' % arr[i]
 		n += 1
-	print '-----------------------------'
-	print 'min number is %s' % arr[0]
-	print '-----------------------------'
-	n +=1
+		print '-----------------------------'
+		print 'Total min number is %s' % arr[0]
+		print '-----------------------------'
+
+	n += 1
 
 print arr
 print n
