e520561ad4efd5caf22c83f4c2aa9b5e8157ae71
nmshuishui <353025240@qq.com>
2016-08-01 17:42:29 +0800

    homework_05

---
+++ 05/zhangyongwei/db.py
@@ -0,0 +1,35 @@
+#encoding: utf-8
+
+def login_required(username, password):
+    with open('db.txt') as f:
+        for user in f:
+            if user.split(':')[1].split(',')[0] == username and user.split(':')[2].strip('}\n') == password:
+                return '登录成功'
+        return '登录失败，用户或密码错误'
+
+def add_user(username, password):
+    if check_user(username):
+        return "用户名已存在"
+    elif username == '' or password == '':
+        return '用户名和密码不能为空'
+    else:
+        with open('db.txt', 'a') as f:
+            f.write("{'name':%s,'password':%s}\n" %(username, password))
+        return '用户添加成功'
+
+def check_user(username):
+    with open('db.txt') as f:
+        for user in f:
+            if user.split(':')[1].split(',')[0] == username:
+                return True
+            else:
+                return False
+
+def uesrs():
+    users = []
+    with open('db.txt') as f:
+        for user in f:
+            username = user.split(':')[1].split(',')[0]
+            password = user.split(':')[2].strip('}\n')
+            users.append({'username':username, 'password':password})
+    return users
---
+++ 05/zhangyongwei/manage.py
@@ -0,0 +1,40 @@
+#coding: utf-8
+from flask import Flask, request, render_template, redirect
+import db
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return redirect('/login/')
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/login_required/', methods=['GET'])
+def login_required():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    return db.login_required(username, password)
+
+@app.route('/regedit/')
+def regedit():
+    return render_template('add_user.html')
+
+@app.route('/add_user/', methods=['GET'])
+def add_user():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    return db.add_user(username, password)
+
+@app.route('/users/')
+def users():
+    users = db.uesrs()
+    return render_template('users.html', users=users)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
