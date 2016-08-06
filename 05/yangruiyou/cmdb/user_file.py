39626e5a76b644e3aafaea5268ff9a29cbf7c2ab
ruiyou.yang <ruiyou.yang@gmail.com>
2016-08-06 15:00:54 +0800

    zuoye

--- 05/yangruiyou/cmdb/user_file.py
+++ 05/yangruiyou/cmdb/user_file.py
@@ -1,20 +1,38 @@
-#coding:utf-8
+# coding:utf-8
 import pickle
 
 
-def Create():
+def Create_v1():
     users = {'panda': '123456', 'woniu': '123456', 'kk': 'kk'}
     fo = open('users.txt', 'wb')
     pickle.dump(users, fo)
     fo.close()
 
 
-def Delete():
+def Create(username, password):
+    users = {username: password}
+    fo = open('users.txt', 'wb')
+    pickle.dump(users, fo)
+    fo.close()
+
+
+def Delete_v1():
     content = {}
     f = open('users.txt')
     content = pickle.load(f)
     f.close()
     content.pop('kk')
+    f = open('users.txt', 'wb')
+    pickle.dump(content, f)
+    f.close()
+
+
+def Delete(username):
+    content = {}
+    f = open('users.txt')
+    content = pickle.load(f)
+    f.close()
+    content.pop(username)
     f = open('users.txt', 'wb')
     pickle.dump(content, f)
     f.close()
@@ -40,22 +58,22 @@ def Select():
     for k, v in content.items():
         print "用户信息:%s--->%s" % (k, v)
 
+
 def SelectOne(username):
     name = username
     content = {}
     userinfo = {}
-    f=open('users.txt')
-    content =pickle.load(f)
+    f = open('users.txt')
+    content = pickle.load(f)
     f.close()
-    userinfo[name]=content[name]
+    userinfo[name] = content[name]
     print userinfo
     return userinfo
 
 
-Create()
-Delete()
-Modify()
-Select()
-SelectOne('panda')
-
-
+if __name__ == '__main__':
+    Create('yangruiyou', '123456')
+    # Delete()
+    # Modify()
+    Select()
+    # SelectOne('panda')
