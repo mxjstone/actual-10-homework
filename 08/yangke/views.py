ef97684741803aca513cd0d5c3b79739c462a09a
jackkeyang <root@localhost.localdomain>
2016-08-26 01:42:33 +0800

    change password

--- 08/yangke/views.py
+++ 08/yangke/views.py
@@ -5,7 +5,7 @@
 
 from run import app
 from flask import render_template,request,redirect,session
-from db import adduser,userlist,delete,getone,modfiy,checkuser
+from db import adduser,userlist,delete,getone,modfiy,checkuser,modpasswd
 from functools import wraps
 import json
 
@@ -24,22 +24,22 @@ def login():
         return render_template("login.html")
     if request.method == "POST":
         login_info = dict((k,v[0]) for k,v in dict(request.form).items())
-        print login_info
         if not login_info["name"] or not login_info["password"]:
             errmsg = "username or password or role not null"
             data = json.dumps({"tag":0,"msg":errmsg})
-            print type(data)
             return data
         if login_info["name"] not in [ n.values()[0] for n in userlist(["name"]) ]:
             errmsg = "user is not exist"
             data = json.dumps({"tag":0,"msg":errmsg})
             return data
-        if login_info["password"] != checkuser(login_info["name"]):
+        if login_info["password"] != checkuser({'name':login_info["name"]}):
             errmsg = "password error"
             data = json.dumps({"tag":0,"msg":errmsg})
             return data
         else:
+            u_role = [ u['role'] for u in userlist(["name","role"]) if u['name'] == login_info["name"] ][0]
             session["username"] = login_info["name"]
+            session["role"] = u_role
             data = json.dumps({"tag":1})
             return data
 
@@ -47,9 +47,15 @@ def login():
 @login_required
 def index():
     fields = ["id","name","name_cn","mobile","email","role","status"]
-    data = userlist(fields)
     username = session.get("username")
-    return render_template("index.html",users = data, username=username)
+    u_role = session.get("role")
+    if u_role == "admin":
+        data = userlist(fields)
+        return render_template("index.html",users = data, username=username)
+    else:
+        uid = [ u['id'] for u in userlist(['id','name']) if u['name'] == username ][0]
+        data = getone(uid)
+        return render_template("self.html",users = data, username=username)
 
 @app.route("/adduser",methods=["GET","POST"])
 @login_required
@@ -99,6 +105,30 @@ def update():
         print userinfo
         modfiy(userinfo)
         return "ok"
+
+@app.route("/cgpassword",methods=["GET","POST"])
+@login_required
+def cgpasswd():
+    if request.method == "GET":
+        uid = request.args.get("id")
+        return render_template("cgpasswd.html",uid=uid)
+    if request.method == "POST":
+        passwd_dic = dict((k,v[0]) for k ,v in dict(request.form).items())
+        password = checkuser({"id":passwd_dic["id"]})
+        print passwd_dic
+        print password
+        if passwd_dic["o_password"] != password:
+            errmsg = "password is error"
+            data = json.dumps({"tag":0,"msg":errmsg})
+            return data
+        if passwd_dic["n_password"] != passwd_dic["r_password"]:
+            errmsg = "The two passwords you typed do not match"
+            data = json.dumps({"tag":0,"msg":errmsg})
+            return data
+        pw_data={"id":passwd_dic['id'],"password":passwd_dic['n_password']}
+        modpasswd(pw_data)
+        data=json.dumps({"tag":1})
+        return data
 
 @app.route("/logout")
 def logout():
