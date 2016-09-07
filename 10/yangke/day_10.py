fb587040622b9fc56f5a36531a531685248b196a
jackkeyang <jack_keyang@163.com>
2016-09-07 22:56:00 +0800

    add ucenter

--- 10/yangke/day_10.py
+++ 10/yangke/day_10.py
@@ -6,18 +6,20 @@ import json
 app = Flask(__name__)
 app.secret_key = "dfahiu2jf"
 
+
+
 @app.route("/login",methods=["GET","POST"])
 def login():
     if request.method == "GET":
         return render_template("login.html")
     if request.method == "POST":
         login_info = dict((k, v[0]) for k, v in dict(request.form).items())
-        if not checkuser({"name": login_info["name"]}, "name"):
+        if not getone({"name":login_info["name"]}):
             return json.dumps({"code": 0, "result":"user is not exist"})
-        if login_info["password"] != checkuser({'name': login_info["name"]})[0]:
+        if login_info["password"] != getone({'name': login_info["name"]})["password"]:
             return json.dumps({"code": 0, "result":"password error"})
 
-        u_role = checkuser({"name": login_info["name"]}, "role")
+        u_role = getone({"name":login_info["name"]})["role"]
         session["username"] = login_info["name"]
         session["role"] = u_role
         return json.dumps({"code": 1,"result":"login success"})
@@ -27,7 +29,7 @@ def login():
 def index():
     if not session.get("username",None):
         return redirect("/login")
-    return render_template("index.html")
+    return render_template("index.html",info=session)
 
 @app.route('/userlist')
 def user_list():
@@ -40,10 +42,10 @@ def user_list():
 @app.route('/add',methods=["GET","POST"])
 def add():
     if request.method == "GET":
-        return render_template("add.html")
+        return render_template("add.html",info=session)
     else:
         addinfo = dict((k, v[0]) for k, v in dict(request.form).items())
-        if checkuser({"name": addinfo["name"]}, "name"):
+        if getone({"name":addinfo["name"]}):
             return json.dumps({"code":1,"errmsg":"user is exist"})
         adduser(addinfo)
         return json.dumps({"code":0,"result":"add user success"})
@@ -60,24 +62,58 @@ def dele():
 def up():
     if request.method == "GET":
         uid = request.args.get("id")
-        userinfo = getone(uid)
+        userinfo = getone({"id":uid})
         return json.dumps(userinfo)
     else:
         userinfo = dict((k,v[0]) for k,v in dict(request.form).items())
         modfiy(userinfo)
         return json.dumps({"code":0})
 
+@app.route('/modfpasswd',methods=["POST"])
+def uppasswd():
+    data = dict((k,v[0]) for k,v in dict(request.form).items())
+    if len(data) == 3:
+        if data["password"] != data["re_password"]:
+            return json.dumps({"code":1,"errmsg":"The tow password is different"})
+        data.pop("re_password")
+        print data
+        modpasswd(data)
+        return json.dumps({"code":0,"result":"Successful modification"})
+    elif len(data) == 4:
+        dic = {}
+        if data["password"] != getone({"name":data["name"]})["password"]:
+            return json.dumps({"code":1,"errmsg":"The password is error"})
+        elif data["new_password"] != data["re_password"]:
+            return json.dumps({"code":1,"errmsg":"The tow password is different"})
+        dic["name"] = data["name"]
+        dic["password"] = data["new_password"]
+        modfiy(dic)
+        return json.dumps({"code":0,"result":"Successful modification"})
+
+@app.route('/ucenter')
+def ucenter():
+    username = session.get("username",None)
+    userinfo = getone({"name":username})
+    del userinfo["password"]
+    return render_template("ucenter.html",uinfo=userinfo,info=session)
+
 @app.route('/idc')
 def idc():
-    return render_template("idc.html")
+    return render_template("idc.html",info=session)
 
 @app.route('/server')
 def server():
-    return render_template("server.html")
+    return render_template("server.html",info=session)
 
 @app.route('/cabinet')
 def cabinet():
-    return render_template("cabinet.html")
+    return render_template("cabinet.html",info=session)
+
+@app.route('/logout/')
+def logout():
+    session.pop("username",None)
+    session.pop("role",None)
+    return redirect("/login")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8000,debug=True)
--- 10/yangke/db.py
+++ 10/yangke/db.py
@@ -9,6 +9,7 @@ conn = mysql.connect(host="localhost",user="root",passwd="123456",db="reboot10",
 conn.autocommit(True)
 curs = conn.cursor()
 
+#获取用户信息列表
 def userlist(fields):
     sql = "select %s from users"%",".join(fields)
     curs.execute(sql)
@@ -16,49 +17,54 @@ def userlist(fields):
     u_list = [dict((k,row[i]) for i, k in enumerate(fields)) for row in result]
     return u_list
 
-def getone(uid):
-    fields = ["id","name","name_cn","mobile","email","role"]
-    sql = "select %s from users where id=%s"%(",".join(fields),uid)
+'''
+    获取单个用户信息，可通过id或用户名查询该用户信息
+    getone({"id":1})
+    getone({"name":"admin"})
+
+'''
+def getone(dic):
+    fields = ["id","name","name_cn","password","mobile","email","role"]
+    sql = "select %s from users where %s='%s'"%(",".join(fields),dic.keys()[0],dic.values()[0])
+    print sql
     curs.execute(sql)
     result = curs.fetchone()
-    u_dict = dict((k, result[i])for i, k in enumerate(fields))
-    return u_dict
+    if result:
+        u_dict = dict((k, result[i])for i, k in enumerate(fields))
+        return u_dict
+    else:
+        return False
 
+#修改用户信息
 def modfiy(fields):
+    print fields
     data = ",".join(["%s='%s'"%(k,v) for k,v in fields.items()])
     sql = "update users set %s where name='%s'"%(data,fields["name"])
     curs.execute(sql)
     conn.commit()
 
+#添加用户
 def adduser(fields):
     sql = "insert into users(%s)values('%s')"%(",".join(fields.keys()),"','".join(fields.values()))
     curs.execute(sql)
     conn.commit()
 
+#删除用户
 def delete(uid):
     sql = "delete from users where id=%s"%uid
     curs.execute(sql)
     conn.commit()
 
-def checkuser(dict,field='password'):
-    sql = "select %s from users where %s='%s'"%(field,dict.keys()[0],dict.values()[0])
-    curs.execute(sql)
-    result = curs.fetchall()
-    if len(result):
-        result = [ v[0] for v in result]
-    else:
-        result = []
-    return result
-
 def modpasswd(dict):
     sql = "update users set password='%(password)s' where id=%(id)s"%dict
+    print sql
     curs.execute(sql)
     conn.commit()
 
 if __name__ == "__main__":
 #    field = ["role"]
     #fields = ["id","name","name_cn","mobile","email","role","status"]
-    print checkuser({"name":"adsdfmin"},"name")
+
 #    print checkuser({"name":"admin"},",".join(field))
     # delete(10)
     #print userlist(['id','name'])
