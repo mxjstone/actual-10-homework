6257940b6fab76da544a161cb8859f77d11e9e89
OmegaEinstein <894773488@qq.com>
2016-09-28 00:18:15 -0700

    homework11

---
+++ 11/yuliangyu/app/cmdb.py
@@ -0,0 +1,149 @@
+from flask import Flask,request,render_template,session,redirect
+from . import app
+import traceback
+import json 
+import db
+import hashlib
+
+#app.secret_key = 'sldkfjljl'
+salt = "sdfljal"
+
+
+@app.route('/idc')
+def idc():
+    if not session.get('name',None):
+	return redirect('/login')
+    fields = ['id','name','name_cn','address','contact','mobile','num']
+    table = 'idc'
+    result = db.selectAll(fields,table)
+    data = [dict((k,row[i]) for i,k in enumerate(fields)) for row in result]
+    return render_template('idc.html',idcs=data,info=session)
+
+@app.route('/addidc',methods=['GET','POST'])
+def addidc():
+    if not session.get('name',None):
+        return redirect('/login')
+#    if session.get('role') != 'admin':
+#        errmsg = "you are not admin, can not register idc"
+#	return json.dumps({'code':1,'errmsg':'you are not admin, can not add idc'})
+    if request.method == 'POST':
+	print '---------reuqst.form-----'
+	print request.form
+	print '---------dict(reuqst.form)-----'
+	print dict(request.form)
+	print '---------dict(reuqst.form).items()-----'
+	print dict(request.form).items()
+        data = {}
+        data = dict((k,v[0]) for k,v in dict(request.form).items())
+        print data
+        # data = request.get_json()
+        fields = ['name','name_cn','address','contact','mobile','num']
+	table = 'idc'
+        if not data["name"]:
+            errmsg = "name null"
+            return render_template("idc.html", result=errmsg)
+        try:
+            db.insert(fields,data,table)
+	    return json.dumps({'code':0,'result':'add idc success'})
+        except:
+            print traceback.print_exc()
+	    return json.dumps({'code':1,'errmsg':'add idc error'})
+    else:
+        return render_template("addidc.html", info = session)
+
+@app.route('/delidc',methods=['GET'])
+def delidc():
+#    if not session.get('username',None):
+#        return redirect('/login')
+    id = request.args.get('id')
+    print id
+    table = 'idc'
+    try:
+        db.delete(id,table)
+	return json.dumps({'code':0, 'result':'delete idc success'})
+    except:
+        print traceback.print_exc()
+	return json.dumps({'code':1, 'result':'delete idc failed'})
+    
+
+@app.route('/updateidc', methods=['GET','POST'])
+def updateidc():
+    table = 'idc'
+    if not session.get('name',None):
+        return redirect('/login')
+    if request.method == "POST":
+        data = dict(request.form)
+	print data
+	print data['id'][0]
+        condition = [ "%s='%s'" % (k,v[0]) for k,v in data.items() ]
+	print condition
+        try:
+            db.update(condition, data['id'][0], table)
+	    return json.dumps({'code':0,'result':'update idc success'})
+        except:
+            print traceback.print_exc()
+	    return json.dumps({'code':1,'result':'update idc failed'})
+    else:
+        id = request.args.get('id')
+	print id
+        fields = ['id', 'name','name_cn','address','contact','mobile','num']
+        try:
+            res = db.selectId(fields,id,table)
+            idc = dict((k,res[i]) for i,k in enumerate(fields))
+	    print idc
+	    return json.dumps({'code':0, 'result':idc})
+        except:
+            print traceback.print_exc()
+	    return json.dumps({'code':1, 'result':'select idcinfo failed'})
+
+    
+@app.route('/cabinet')
+def cabinet():
+    if not session.get('name',None):
+	redirect('/login')
+    fields = ['id','name','idc_id','u_num','os','power']
+    table = 'cabinet'
+    try:
+        result = db.selectAll(fields,table)
+	print result
+        data = [dict((k,row[i]) for i,k in enumerate(fields)) for row in result]
+	print data
+        return  render_template('cabinet.html', cabinets = data, info = session)
+    except:	
+        print traceback.print_exc()
+        return json.dumps({'code':1,'result':'select cabinet failed'})
+
+
+@app.route('/addcabinet',methods=['GET','POST'])
+def addcabinet():
+    if session.get('name',None):
+	redirect('/login')
+    if request.method == 'POST':
+	data = dict((k,v[0]) for k,v in dict(request.form).items())
+	fields = ['name','idc_id','u_num','power']
+	table = 'cabinet'
+	try:
+	    db.insert(fields,data,table)
+	    return json.dumps({'code':0, 'result': 'add cabinet success'})
+	except:
+            print traceback.print_exc()
+	    return json.dumps({'code':1,'errmsg':'add cabinet error'})
+	    
+    else:
+	table = 'idc'
+	fields = ['id','name']
+	result = db.selectAll(fields,table)
+	print result
+	data = [ dict((k,row[i]) for i,k in enumerate(fields)) for row in result ]
+	print data
+	return render_template('addcabinet.html',info = session, idcs = data)
+
+@app.route('/updatecab',methods=['GET','POST'])
+def updatacab():
+    if session.get('name',None):
+	redirect('/login')
+
+
+@app.route('/server')
+def server():
+    return render_template('server.html')
---
+++ 11/yuliangyu/app/user.py
@@ -0,0 +1,121 @@
+from flask import Flask,request,render_template,session,redirect
+from . import app
+import traceback
+import json 
+import db
+import hashlib
+
+#app.secret_key = 'sldkfjljl'
+salt = "sdfljal"
+
+@app.route('/')
+@app.route('/index')
+def index():
+    if not session.get('name',None):
+	return redirect('/login')
+    fields = ['id','name','name_cn','mobile','email','role','status']
+    condition = 'name = "%(name)s"' % session
+    print condition
+    print session
+    result = db.selectOne(fields,condition)
+    print result
+    user = dict((k,result[i]) for i,k in enumerate(fields))
+    return  render_template('index.html', info = session, user = user)
+
+@app.route('/userlist')
+def userlist():
+    if not session.get('name',None):
+	return redirect('/login')
+    fields = ['id','name','name_cn','mobile','email','role','status']
+    table = 'users'
+    result = db.selectAll(fields,table)
+    data = [dict((k,row[i]) for i,k in enumerate(fields)) for row in result]
+    return render_template('userlist.html',users=data,info=session)
+
+
+@app.route('/add',methods=['GET','POST'])
+def add_user():
+    if not session.get('name',None):
+        return redirect('/login')
+    if session.get('role') != 'SU':
+	return json.dumps({'code':0, 'result':'you are not admin, can not register user'})
+    if request.method == 'POST':
+        data = {}
+        data = dict((k,v[0]) for k,v in dict(request.form).items())
+	data['password'] = hashlib.md5(data['password']+salt).hexdigest()
+        print data
+        # data = request.get_json()
+        fields = ['name','name_cn','password','mobile','email','role','status']
+        if not data["name"]: 
+	    return json.dumps({'code':1, 'result':'name not null'})
+        try:
+	    table = 'users'
+            db.insert(fields,data,table)
+	    return json.dumps({'code':0, 'result':'add user success'})
+        except:
+            print traceback.print_exc()
+	    return json.dumps({'code':1, 'result':'name not null'})
+    else:
+        return render_template("addidc.html",info=session)
+
+@app.route('/update', methods=['GET','POST'])
+def update():
+    table = 'users'
+    if not session.get('name',None):
+        return redirect('/login')
+    if request.method == "POST":
+        data = dict(request.form)
+	data['password'][0] = hashlib.md5(data['password'][0]+salt).hexdigest()
+	print data
+        condition = [ "%s='%s'" % (k,v[0]) for k,v in data.items() ]
+	print condition
+        try:
+            db.update(condition, data['id'][0], table)
+	    return json.dumps({'code':0,'result':'update user success'})
+        except:
+            print traceback.print_exc()
+	    return json.dumps({'code':1,'result':'update user failed'})
+    else:
+        id = request.args.get('id')
+        fields = ['id', 'name', 'name_cn', 'email', 'mobile']
+        try:
+            res = db.selectId(fields,id,table)
+            user = dict((k,res[i]) for i,k in enumerate(fields))
+	    return json.dumps({'code':0, 'result':user})
+        except:
+            print traceback.print_exc()
+	    return json.dumps({'code':1, 'result':'select userinfo failed'})
+
+@app.route('/delete', methods=['GET'])
+def delete():
+    if not session.get('name',None):
+        return redirect('/login')
+    id = request.args.get('id')
+    table = 'users'
+    try:
+        db.delete(id,table)
+	return json.dumps({'code':0, 'result':'delete user success'})
+    except:
+        print traceback.print_exc()
+	return json.dumps({'code':1, 'result':'delete user failed'})
+
+@app.route('/chpwdoneself',methods=['POST'])
+def chpwdoneself():
+    if not session.get('name',None):
+        return redirect('/login')
+    if request.method == "POST":
+        data = dict((k,v[0]) for k ,v in dict(request.form).items())
+	fields = ['password']
+	id = data['id']
+        password = db.selectId(fields,id,'users')
+        print data
+        print password
+        if data["oldpasswd"] != password[0]:
+            return json.dumps({"code":1,"errmsg":"password is error"})
+	try:
+    	    db.updatepwd(data['newpasswd'],id)
+	    return json.dumps({'code':0, 'result':'update password success'})
+        except:
+            print traceback.print_exc()
+	    return json.dumps({'code':1, 'errmsg':'update password failed'})
+    
