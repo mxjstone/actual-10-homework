5499a4ea4d0b1b268c20902fd9d163ed651a65a0
root <yaoliang83@yeah.net>
2016-11-04 09:46:38 +0800

    add code update

--- 12/yaoliang/app/__init__.py
+++ 12/yaoliang/app/__init__.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 app.secret_key='\xd1\xfe\xfb\x7fH\xbf\xc8Q\x17-\xab\xc6\x80u2\xc2\xb6\n9\x00\x87\xa7\xa75'
 
 
-import user,idc,cabinet,server,job
+import user,idc,cabinet,server,job,code
--- 12/yaoliang/config.py
+++ 12/yaoliang/config.py
@@ -11,10 +11,16 @@ class Table():
     FIELDS_IDC=['id','name','name_cn','address','adminer','phone','num']
     FIELDS_CABINET=['id','name','idc_id','u_num','power']
     FIELDS_SERVER=['id','hostname','lan_ip','wan_ip','cabinet_id','op','phone']
-    #FIELDS_WORK=['id','des_name','work','work_status','handle_name','operate','operate_bak','des_time','handle_time']
+    FIELDS_CODE=['id','update_date','package','update_persion','project','message']
     FIELDS_OPS_JOBS=['id','apply_date','apply_type','apply_desc','deal_persion','status','deal_desc','deal_time','apply_persion']
 
 class MyEmail():
     # 运维组邮箱账号
     LOCAL_EMAIL='yaoliang83@yeah.net'
     LOCAL_PASSWD='******'
+
+class RemoteHost():
+#    HOST1=['192.168.1.100',22,'root','123456']
+#    HOST2=['192.168.1.101',22,'root','123456']
+#    HOST3=['192.168.1.102',22,'root','123456']
+    HOST1=['192.168.38.250',22,'root','890iop']
