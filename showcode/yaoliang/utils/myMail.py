d8f7ba924e49f11946e55a12f1b345754337a98e
yaoliang83 <yaoliang83@yeah.net>
2016-12-08 13:50:16 +0800

    Update myMail.py

--- showcode/yaoliang/utils/myMail.py
+++ showcode/yaoliang/utils/myMail.py
@@ -3,8 +3,8 @@ from smtplib import SMTP
 
 
 def myMail(from_email,passwd,to_email,content):
-#    SMTPSVR = SMTP('smtp.yeah.net')
-    SMTPSVR = SMTP('smtp.sina.com')
+    SMTPSVR = SMTP('smtp.yeah.net')
+#    SMTPSVR = SMTP('smtp.sina.com')
 
     body = '''\From: {}\r\nTo: {}\r\nSubject: 'JOB EMAIL'\r\n\r\n操作结果: {}\r\n'''.format(from_email,to_email,content)
 
@@ -13,5 +13,3 @@ def myMail(from_email,passwd,to_email,content):
     errs = sendSvr.sendmail(from_email,[to_email],body)
     sendSvr.quit()
 
-if __name__=="__main__":
-    myMail('mhealth365@sina.com','Mhealth2017Mail','liang.yao@mhealth365.com','test')
